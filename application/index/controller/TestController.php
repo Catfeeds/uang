3d7abf3cc610a9af0d568ea4fd05f507fcd41cd8
zhengwang <152309246@qq.com>
2017-12-18 17:00:16 +0800

    c

--- application/index/controller/TestController.php
+++ application/index/controller/TestController.php
@@ -86,17 +86,16 @@ class TestController extends Base
     public function sendjson($json = '')
     {
         $data=$json;
-        $curlHandle = curl_init('http://45.32.107.195/sms/api_sms_otp_send_json.php');
-        curl_setopt($curlHandle, CURLOPT_POST, 1);
+        $curlHandle = curl_init('http://45.32.107.195/sms/api_sms_otp_balance_json.php');
+        curl_setopt($curlHandle, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($curlHandle, CURLOPT_POSTFIELDS, $data);
         curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curlHandle, CURLOPT_HTTPHEADER, array(
-                    'Content-Type: application/json; charset=utf-8',
+                    'Content-Type: application/json',
                     'Content-Length: ' . strlen($data))
         );
-        curl_setopt($curlHandle, CURLOPT_TIMEOUT, 30);
-        curl_setopt($curlHandle, CURLOPT_CONNECTTIMEOUT, 30);
-        // ob_start();
+        curl_setopt($curlHandle, CURLOPT_TIMEOUT, 5);
+        curl_setopt($curlHandle, CURLOPT_CONNECTTIMEOUT, 5);
         $respon = curl_exec($curlHandle);
 
         $curl_errno = curl_errno($curlHandle);
